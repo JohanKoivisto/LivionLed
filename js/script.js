749688c033b216e0a94bb62c4ab403b9567bfe5b
JohanKoivisto <60185431+JohanKoivisto@users.noreply.github.com>
2020-11-04 23:58:36 +0200

    real time loading of emojis. styling and hosting left

--- js/script.js
+++ js/script.js
@@ -8,10 +8,10 @@ ctx.fillRect(0, 0, canvas.width, canvas.height)
 let savedEmoji = document.getElementById('saved-emoji')
 
 ledArray = [
-  [1, 0, 0, 0, 0, 0, 0, 0],
-  [0, 1, 0, 0, 0, 0, 0, 0],
-  [0, 0, 1, 0, 0, 0, 0, 0],
-  [0, 0, 0, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0],
@@ -21,7 +21,7 @@ ledArray = [
 // Define look for led block
 const block = (x, y, width, height, color, contxt = ctx) => {
   contxt.fillStyle = color
-  contxt.fillRect(x, y, width, height);
+  contxt.fillRect(x, y, width, height)
 }
 
 // Get coordinates, map to ledArray, toggle block on/off and call draw function to update DOM
@@ -62,34 +62,61 @@ const parseLedArray = str => {
   return JSON.parse(str)
 }
 
-console.log(arrToJSON(ledArray));
+// set grid to initial state
+const initalizeLedArray = () => {
+  ledArray = ledArray.map(el => [0, 0, 0, 0, 0, 0, 0, 0])
+}
 
 // save emojis to database
 const saveEmoji = () => {
   event.preventDefault()
   let emojiName = document.getElementById('emoji-name').value
-  db.collection('emojis').add({
-    name: emojiName,
-    emojimap: arrToJSON(ledArray)
-  })
+  if (emojiName === "") {
+    alert("you must give your emoji a name");
+  } else {
+    db.collection('emojis').add({
+      name: emojiName,
+      emojimap: arrToJSON(ledArray)
+    })
+  }
+  
+}
+// create minicanvas to display saved emoji in a grid
+const createMiniCanvas = doc => {
+  // create Dom elements
+  const newDiv = document.createElement("div")
+  const newCanvas = document.createElement('canvas')
+  const newP = document.createElement("p")
+  
+  // add classlist and define canvas
+  newDiv.classList.add("minicanvas")
+  newP.textContent = doc.data().name;
+  newCanvas.id = doc.id
+  newCanvas.width = 120
+  newCanvas.height = 120
+  let cty = newCanvas.getContext('2d')
+  cty.fillStyle = '#2d2d2d'
+  cty.fillRect(0, 0, newCanvas.width, newCanvas.height)
+  ledArray = parseLedArray(doc.data().emojimap)
+
+  // append everything to emoji div
+  savedEmoji.appendChild(newDiv)
+  newDiv.appendChild(newCanvas)
+  newDiv.appendChild(newP)
+  
+  //draw minicanvas
+  drawLedDisplay(cty, 14, 14, 120, 120)
 }
 
-// get data
-db.collection('emojis')
-  .get()
-  .then(snapshot => {
-    snapshot.docs.forEach(doc => {
-      const newCanvas = document.createElement('canvas')
-      newCanvas.id = doc.id
-      newCanvas.width = 120
-      newCanvas.height = 120
-      savedEmoji.appendChild(newCanvas)
-      let cty = newCanvas.getContext('2d')
-      cty.fillStyle = '#2d2d2d'
-      cty.fillRect(0, 0, newCanvas.width, newCanvas.height)
-      ledArray = parseLedArray(doc.data().emojimap)
-      drawLedDisplay(cty, 14, 14, 120, 120)
-    })
+// get emoji data and render each one to minicanvas, listen to changes
+db.collection("emojis").orderBy("name").onSnapshot(snapshot => {
+  let changes = snapshot.docChanges();
+  changes.forEach(change => {
+    if (change.type === "added") {
+      createMiniCanvas(change.doc)
+    }
+    initalizeLedArray()
   })
+})
 
-  drawLedDisplay(ctx, 59, 59, 480, 480)
+drawLedDisplay(ctx, 59, 59, 480, 480)
